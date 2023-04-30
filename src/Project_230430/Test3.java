f2e4793d5720bc5d0807234bddc19c67c96042cb
mwkim <pizzas08polite@icloud.com>
2023-04-30 15:47:50 +0900

    raw source code

--- src/Project_230430/Test3.java
+++ src/Project_230430/Test3.java
@@ -8,14 +8,25 @@ import java.nio.file.StandardOpenOption;
 public class Test3 {
     public static void main(String[] args) {
         try{
+            //"/Users/kimmyeong-wan/IdeaProjects/Project_230430/test" 디렉토리를
+            //File 객체로 가리킨다.
             String name = "/Users/kimmyeong-wan/IdeaProjects/Project_230430/test";
             File dir = new File(name);
+
+            //File 객체가 가리키는 디렉토리 안의 디렉토리,파일들을 이름을
+            // 반환타입이 String인 fileName배열에 넣는다.
             String[] fileName = dir.list();
+
+            //넣고싶은 문장을 생성한후 바이트로 변환하고 byteArr배열에 넣는다.
+            String lineToAppend = "문장삽입확인1";
+            byte[] byteArr = lineToAppend.getBytes();
+
+            //fileName배열에 들어있는 디렉토리와 파일의 이름을 차례대로 s에 대입한다.
             for(String s : fileName){
+                //파일 확장자가 txt인 파일들만 수정하기 위해 if문을 사용해서
+                //파일 이름의 끝 세글자가 txt일때만 문장을 삽입한다.
                 int end = s.length();
-                String lineToAppend = "\r\nThe quick brown fox jumps over the lazy dog";
-                byte[] byteArr = lineToAppend.getBytes();
-                if(s.substring(end-2,end).equals("xt")){
+                if(s.substring(end-3,end).equals("txt")){
                     //Path.get에는 절대경로가 들어와야 한다.
                     //Path.get(name+"/"+s)
                     //Files.write(Paths.get(s), byteArr, StandardOpenOption.APPEND);
--- src/Project_230430/Test4.java
+++ src/Project_230430/Test4.java
@@ -12,17 +12,28 @@ public class Test4 {
         Scanner scanner = new Scanner(System.in);
 
         try {
+            //"/Users/kimmyeong-wan/IdeaProjects/Project_230430/test" 디렉토리를
+            //File 객체로 가리킨다.
             String name = "/Users/kimmyeong-wan/IdeaProjects/Project_230430/test";
             File dir = new File(name);
-            String lineToAppend = scanner.nextLine();
+
+            //File 객체가 가리키는 디렉토리 안의 디렉토리,파일들을
+            // 반환타입이 File 객체인 fileName배열에 넣는다.
+            File[] filename = dir.listFiles();
+
+            //넣고싶은 문장을 생성한후 바이트로 변환하고 byteArr배열에 넣는다.
+            String lineToAppend = "문장 삽입 확인2";
             byte[] byteArr = lineToAppend.getBytes();
 
-            File[] filename = dir.listFiles();
+
             for (File s : filename) {
                 int end = s.getName().length();
-                if (s.getName().substring(end - 2, end).equals("xt")) {
+                //확장자가 txt인 파일만 수정하는 알고리즘
+                if(s.isDirectory()){
+                    continue;
+                }
+                if (s.getName().substring(end - 3, end).equals("txt")) {
                     Files.write(Paths.get(s.getAbsolutePath()), byteArr, StandardOpenOption.APPEND);
-                    System.out.println(s+"에 "+lineToAppend+"를 삽입했습니다.");
                 }
             }
         }catch (Exception e){
